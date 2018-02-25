157979fc0f91e1419f4d1018b001c7a6ca65824d
Kate Makarenka <polumna-la@mail.ru>
2018-02-26 01:44:41 +0300

    solution2

--- src/index.js
+++ src/index.js
@@ -21,20 +21,29 @@ class Sorter {
     return this.memory
   }
 
-  sort(indices) {
-
-    let temp = this.memory.map((v,i) => [v,i]);
-    temp.sort(function(a,b){
-      if(indices.indexOf(a[1]) != -1 && indices.indexOf(b[1]) !== -1){
-        return a[0]-b[0];
-        }
-      }
-    );
-    this.memory = temp.map((v) => v[0])
+  sort(indices) {   
+    var temp = []
+    for(let i=0;i<indices.length;i++){
+      temp.push(this.memory[indices[i]])
+    }    
+    if(this.comparator){
+      temp.sort(this.comparator)
+    } else {
+      temp.sort(function(a,b){
+        return a-b
+      });
+    }    
+    if(indices[0] > indices[1]){
+      temp.reverse()
+    }    
+    for(let j = 0; j<temp.length;j++){
+      this.memory[indices[j]] = temp[j]
+    }      
   }
 
   setComparator(compareFunction) {
     this.comparator = compareFunction
+    
   }
 }
 
